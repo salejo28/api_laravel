2a468a2d664e978beab7aed1f119d30f9808445d
Santiago <santiago.aruiz28@gmail.com>
2021-04-20 17:06:48 -0500

    Add tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Vehiculo;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -21,8 +22,44 @@ class ExampleTest extends TestCase
 
     public function test_get_vehicles()
     {
-        $response = $this->get('/vehiculos');
+        $response = $this->get('/api/vehiculos');
 
-        $response->assertOk();
+        $response->assertStatus(200);
+    }
+
+    public function test_get_vehicle_by_brand()
+    {
+        // The brands are Mazda Honda Akt Yamaha Chevrolet Renault Mazda
+        $response = $this->get('/api/vehiculo/brand/Mazda');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_get_vehicle_by()
+    {
+        // Get vehicle by propietarie or identification (cedula) or license plate (placa)
+        $response = $this->get('/api/vehiculo/santiago');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_create_vehicle()
+    {
+        // type vehicle carro, camioneta or moto
+        // Brands is carro or camioneta (Mazda, Chevrolet, Renault) is moto (Honda, Akt, Yamaha)
+        $this->post('/api/vehiculo', [
+            'placa' => 'abd123',
+            'tipo_vehiculo' => 'carro',
+            'marca' => 'Mazda',
+            'propietario' => 'Santiago',
+            'cedula_propietario' => '1000954519'
+        ]);
+
+        $this->assertCount(1, Vehiculo::all());
+        $vehiculo = Vehiculo::first();
+
+        $this->patch('/api/vehiculo' . $vehiculo->id, [
+            'placa' => 'New Vehicle',
+        ]);
     }
 }
